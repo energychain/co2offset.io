b0ba737edb6541fc6c0a179f9e788cb0f087c4d3
Thorsten Zoerner <me@thorsten-zoerner.com>
2021-09-06 10:25:19 +0200

    added JSON-LD

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,20 @@
 $('document').ready(function() {
-  $('#getQuote').click(function() {
+  const getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
+  const retrieveQuote = function() {
     $.getJSON("https://api.corrently.io/v2.0/co2/price?co2="+$('#co2eq').val(),function(data) {
       $('#co2req').html(data.reqCO2);
       $('#co2get').html(data.getCO2);
@@ -9,8 +24,12 @@ $('document').ready(function() {
             location.href = data ;
           })
       })
+      $('#priceModal').modal("show");
     })
-  })
+  }
+
+
+  $('#getQuote').click(retrieveQuote)
 
   const ui = SwaggerUIBundle({
     url: "/assets/js/openapi.json",
@@ -25,5 +44,10 @@ $('document').ready(function() {
     ],
     layout: "StandaloneLayout"
   });
+
+  if(getUrlParameter("co2")) {
+    $('#co2eq').val(getUrlParameter("co2"));
+    retrieveQuote();
+  }
 
 });
