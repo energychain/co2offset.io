b3a20d93888705880b5e0fb5aeb1115787198622
Thorsten Zoerner <me@thorsten-zoerner.com>
2021-11-08 00:48:29 +0100

    Removed stalled Plugin

--- build/webpack.app.config.js
+++ build/webpack.app.config.js
@@ -1,18 +1,8 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const base = require("./webpack.base.config");
 
 module.exports = env => {
-  plugins: [
-      new CopyWebpackPlugin({
-          patterns: [
-              { from: '/home/zoernert/Development/co2offset.io', 
-                to: 'app'
-              }
-          ]
-      })
-  ]
   return merge(base(env), {
     entry: {
       main: "./src/main.js",
